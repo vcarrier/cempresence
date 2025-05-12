e253411f0342d9404a08860837bdd32c1bd39dcc
vcarrier <vincent.carrier@cegepmontpetit.ca>
2025-05-12 16:20:03 -0400

    first commit

---
+++ presences.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(ValueFromPipeline)]
+    [ValidateScript({Test-Path -Path $_ -PathType Container})]
+    [string] $Path = (Join-Path -Path $Home -ChildPath "Desktop"),
+    [string] $Cours,
+    [string] $Groupe
+)
+
+if(-not (Get-Module -Name AdsiPS -ListAvailable)) {
+    Install-Module -Name AdsiPS -Scope CurrentUser -Force
+}
+
+$InputFile = New-TemporaryFile
+
+$arguments = @(
+    "/process pcinssui.exe",
+    "/type listview",
+    "/visible Yes",
+    "/scomma $($InputFile.FullName)"
+)
+
+Start-Process -FilePath ".\sysexp.exe" -ArgumentList $arguments -Wait
+
+Import-Csv -Path $InputFile.FullName -Encoding Default | ForEach-Object {
+    if ($sam = $_."Nom d'utilisateur") {
+        [PSCustomObject]@{
+            Username = ($_."Nom d'utilisateur")
+            NomÉtudiant = (Get-ADSIUser -Identity $sam).DisplayName
+            Ordinateur = $_.Nom
+            DateHeure = (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
+            Programme = (Get-ADSIUser -Identity $sam).description
+        }
+    }
+} | Sort-Object -Property NomÉtudiant | Tee-Object -Variable "Rapport" | Format-Table
+
+Write-Host "Nombre de sessions:", $Rapport.count -ForegroundColor "Yellow"
+
+$Rapport | Export-Csv -Path "$Path\Rapport_Présences_$(Get-Date -Format "yyyyMMdd_HHmm").csv" -Encoding UTF8 -UseCulture -NoTypeInformation
+
+$InputFile | Remove-Item -Force
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Script pour la prise de présences
+
+1. Lancer la console tuteur de NetSupport School
+
+2. Sélectionner l'option "Surveiller les saisies de clavier en temps réel" (icône *QWAS*)
+
+3. Faire `[F5]` pour rafraîchir la liste des étudiants
+
+4. Démarrer un terminal PowerShell 
+         (**`[shift]` + `[clic-droit]`** sur le dossier du script, *"Ouvrir la fenêtre PowerShell ici"*)
+
+5. Exécuter `.\presences.ps1`
+
+Le script présente la liste des étudiants et enregistre un rapport en format CSV sur le bureau.
+
